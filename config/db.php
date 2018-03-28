1efb6a7d983e713a65400ff3312a6f2ba0115ae7
Massimiliano Masserelli <massimiliano.masserelli@essepi.eu>
2018-03-28 22:07:32 +0200

    Aggiunto supporto .env

--- config/db.php
+++ config/db.php
@@ -1,14 +1,29 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+if (YII_ENV_PROD) {
+  return [
+      'class' => 'yii\db\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=cdjockey',
+      'username' => 'root',
+      'password' => 'root',
+      'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+      // Schema cache options (for production environment)
+      'enableSchemaCache' => true,
+      'schemaCacheDuration' => 60,
+      'schemaCache' => 'cache',
+  ];
+} else {
+  return [
+      'class' => 'yii\db\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=cdjockey',
+      'username' => 'root',
+      'password' => '',
+      'charset' => 'utf8',
+
+      // Schema cache options (for production environment)
+      // 'enableSchemaCache' => true,
+      // 'schemaCacheDuration' => 60,
+      // 'schemaCache' => 'cache',
+  ];
+}
--- web/index.php
+++ web/index.php
@@ -1,10 +1,11 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Environment
+require __DIR__ . '/../common/env.php';
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
